824085aa19bec3b32adab76d456d075f1b4f77dd
yuming0315 <gjdbal111@naver.com>
2023-02-09 10:48:08 +0900

    site - Spring bean #finish

--- mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -2,7 +2,9 @@ package com.douzone.mysite.controller;
 
 import javax.servlet.ServletContext;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,13 @@ import com.douzone.mysite.vo.SiteVo;
 @RequestMapping("/admin")
 public class AdminController {
 	@Autowired
+	private ServletContext servletContext;
+	
+	@Autowired
 	private SiteService siteService;
+	
+	@Autowired
+	private ApplicationContext applicationContext;
 	
 	@Autowired
 	private FileuploadService fileuploadService;
@@ -36,7 +44,17 @@ public class AdminController {
 	public String update(SiteVo vo, @RequestParam("file") MultipartFile multipartFile) {
 		String url = fileuploadService.restore(multipartFile);
 		vo.setProfile(url);
+		
+		SiteVo site = applicationContext.getBean(SiteVo.class);
+
 		siteService.updateSite(vo);
+		servletContext.setAttribute("sitevo", vo);
+//		site.setTitle(vo.getTitle());
+//		site.setWelcome(vo.getWelcome());
+//		site.setProfile(vo.getProfile());
+//		site.setDescription(vo.getDescription());
+		BeanUtils.copyProperties(vo, site);
+		
 		return "redirect:/admin";
 	}
 	
